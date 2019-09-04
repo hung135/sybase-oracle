617de591a913cfbdd03eada1a37b979b982078b1
Hung Nguyen <hung.nguyen@cfpb.gov>
2019-09-04 12:24:30 +0000

    stubbing out tests scenarios

---
+++ tests/test_sybase_oracle.py
@@ -0,0 +1,30 @@
+from __future__ import unicode_literals
+
+import io
+import os
+
+from pytest import raises
+# This will treat each verifition item as a test scenario objects in Sybase and oracle
+# will run through each test scenario and make sure everything passes
+# as items are identified it will be a test scenario added to this list
+
+
+sybase_conn = None  # need to install sybase python library
+oracle_conn = None  # need to install oracle python libraryyy
+
+def test_constraints():
+    assert False
+def test_functions():
+    assert False
+def test_users():
+    assert False
+def test_indexes():
+    assert False
+def test_collations():
+    assert False
+def test_triggers():
+    assert False
+def test_schemea():
+    assert False
+def test_privs():
+    assert False
