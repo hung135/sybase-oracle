24e4986ee3947a59009b85efbb2f9d5cda35250f
hnguyen <hung135@hotmail.com>
2019-09-05 00:30:50 +0000

    sybase driver - currently not working

--- tests/test_sybase_oracle.py
+++ tests/test_sybase_oracle.py
@@ -7,7 +7,7 @@ from pytest import raises
 # This will treat each verifition item as a test scenario objects in Sybase and oracle
 # will run through each test scenario and make sure everything passes
 # as items are identified it will be a test scenario added to this list
-
+from sqlalchemy import create_engine, inspect
 
 
     
@@ -16,13 +16,17 @@ sybase_conn = None  # need to install sybase python library
 oracle_conn = None  # need to install oracle python libraryyy
 def test_connect_oracle():
     
-    from sqlalchemy import create_engine, inspect
+    
     engine = create_engine("oracle+cx_oracle://system:Docker12345@dboracle/ORCLCDB")
     inspector = inspect(engine)
     assert True
     #all_check_constraints = inspector.get_check_constraints(
     #    "some_table", include_all=True)
-
+def test_connect_sybase():
+    
+        
+    engine = create_engine("sybase+pyodbc://sa:password@dbsybasedsn/guest")
+    inspector = inspect(engine)
 def test_constraints():
     assert False
 def test_functions():
